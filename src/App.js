65342911625f23febf82b28fac2ea2cbb0b38e13
Vinay Matta <vinaymatta63@gmail.com>
2022-04-02 22:20:00 +0530

    added firebase hosting and loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Physics } from "@react-three/cannon";
+import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
 import { PerspectiveCamera } from "three";
 import "./App.css";
-import Welcome from "./components/Welcome";
+import Welcome from "./components/welcome";
 
 function App() {
   const cameraRef = useRef(
@@ -26,13 +27,38 @@ function App() {
         />
       )}
 
-      <Canvas style={{ height: "100vh" }} camera={cameraRef.current}>
+      <Canvas
+        dpr={[1, 1.5]}
+        style={{ height: "100vh" }}
+        camera={cameraRef.current}
+      >
         <Physics gravity={[0, -9.82, 0]}>
           <Suspense fallback={null}>
             <Welcome ref={positionRef} cameraRef={cameraRef} />
           </Suspense>
         </Physics>
       </Canvas>
+      <Loader
+        containerStyles={{
+          background:
+            "radial-gradient(circle farthest-corner at center top,#071021,#19324a)",
+        }} // Flex layout styles
+        innerStyles={{
+          backgroundColor: "salmon",
+          width: "50vw",
+        }} // Inner container styles
+        barStyles={{
+          backgroundColor: "lightgreen",
+        }} // Loading-bar styles
+        dataInterpolation={(p) => `Loading ${Math.round(p)}%`}
+        initialState={(active) => active}
+        dataStyles={{
+          color: "#fafafa",
+          fontSize: "25px",
+          fontFamily: "Raleway",
+          fontWeight: "500",
+        }}
+      />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh5X71bzw4bVAbnJxWniESQuJUm2mbJeY",
+  authDomain: "virtual-shop-vm.firebaseapp.com",
+  projectId: "virtual-shop-vm",
+  storageBucket: "virtual-shop-vm.appspot.com",
+  messagingSenderId: "509172981707",
+  appId: "1:509172981707:web:20e3284d4b17bfb459204e",
+  measurementId: "G-JXK1YK7EWM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
